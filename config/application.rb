bb45bc197f438ffa771ca2b6f0f36fa6ebaa2b90
Oleg <godschildknopik@gmail.com>
2016-04-26 16:57:49 +0300

    Finish user signup

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,7 @@ module SampleApp
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+    html_tag }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
+ 
 
-
+  resources :users
   match '/signup', to: 'users#new', via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
